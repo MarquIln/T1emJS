bcf02967a73451d9681f9d478c9e5fe12be5387a
Marcos <marcosraach.1@gmail.com>
2024-04-10 12:13:16 -0300

    feat: done!

--- index.js
+++ index.js
@@ -1,114 +1,110 @@
 const fs = require('fs')
 
+function executeCase(Characters, time) {
+    const Direction = {
+        RIGHT: 1,
+        LEFT: 2,
+        UP: 3,
+        DOWN: 4
+    }
+
+    let actualLine = 0
+    let actualColumn = 0
+    let partialSum = ""
+    let sum = 0
+    let direction = Direction.RIGHT
+
+    while (Characters[actualLine][actualColumn] !== "-") {
+        actualLine ++
+    }
+    while (Characters[actualLine][actualColumn] !== "#") {
+        switch (direction) {
+            case Direction.RIGHT:
+                if ((parseInt(Characters[actualLine][actualColumn]))) {
+                    while ((parseInt(Characters[actualLine][actualColumn]))) {
+                        partialSum += Characters[actualLine][actualColumn]
+                        actualColumn++
+                    }
+                    sum += parseInt(partialSum)
+                    partialSum = ""
+                }
+                if (Characters[actualLine][actualColumn] === "\\") {
+                    direction = Direction.DOWN
+                    actualLine ++
+                } else if (Characters[actualLine][actualColumn] === "/") {
+                    direction = Direction.UP
+                    actualLine --
+                } else actualColumn++
+                break
+            case Direction.LEFT:
+                if ((parseInt(Characters[actualLine][actualColumn]))) {
+                    while ((parseInt(Characters[actualLine][actualColumn]))) {
+                        partialSum += Characters[actualLine][actualColumn]
+                        actualColumn --
+                    }
+                    sum += parseInt(partialSum)
+                    partialSum = ""
+                }
+                if (Characters[actualLine][actualColumn] === "\\") {
+                    direction = Direction.UP
+                    actualLine --
+                } else if (Characters[actualLine][actualColumn] === "/") {
+                    direction = Direction.DOWN
+                    actualLine ++
+                } else actualColumn --
+                break
+            case Direction.UP:
+                if ((parseInt(Characters[actualLine][actualColumn]))) {
+                    while ((parseInt(Characters[actualLine][actualColumn]))) {
+                        partialSum += Characters[actualLine][actualColumn]
+                        actualLine --
+                    }
+                    sum += parseInt(partialSum)
+                    partialSum = ""
+                }
+                if (Characters[actualLine][actualColumn] === "\\") {
+                    direction = Direction.LEFT
+                    actualColumn --
+                } else if (Characters[actualLine][actualColumn] === "/") {
+                    direction = Direction.RIGHT
+                    actualColumn++
+                } else actualLine --
+                break
+            case Direction.DOWN :
+                if ((parseInt(Characters[actualLine][actualColumn]))) {
+                    while ((parseInt(Characters[actualLine][actualColumn]))) {
+                        partialSum += Characters[actualLine][actualColumn]
+                        actualLine ++
+                    }
+                    sum += parseInt(partialSum)
+                    partialSum = ""
+                }
+                if (Characters[actualLine][actualColumn] === "\\") {
+                    direction = Direction.RIGHT
+                    actualColumn++
+                } else if (Characters[actualLine][actualColumn] === "/") {
+                    direction = Direction.LEFT
+                    actualColumn --
+                } else actualLine ++
+                break
+        }
+    }
+    console.log("Soma total = " + sum)
+    console.log("Tempo de leitura do arquivo: " + time.toFixed(3))
+}
+
 function readFile(name, callback) {
     const startTime = process.hrtime()
-
     fs.readFile(name, 'utf8', (err, data) => {
         if (err) {
             console.error(err)
             return
         }
-        const lineCharacters = data.split('\n').map(line => line.split(''))
+        const Characters = data.split('\n').map(line => line.split(''))
         const endTime = process.hrtime(startTime)
-        const elapsedTime = endTime[0] * 1000 + endTime[1] / 1000000000
-
-        callback(lineCharacters, elapsedTime)
+        const timeInSeconds = endTime[0] + endTime[1] / 100000000
+        callback(Characters, timeInSeconds)
     })
 }
 
-function executeCase(lineCharacters, elapsedTime) {
-    let actualLine = 0
-    let actualPosition = 0
-    let partialSum = ""
-    let sum = 0
-    let direction = 1
-
-    while (lineCharacters[actualLine][actualPosition] !== "-") {
-        actualLine++
-    }
-
-    while (lineCharacters[actualLine][actualPosition] !== "#") {
-        switch (direction) {
-            case 1:
-                if ((parseInt(lineCharacters[actualLine][actualPosition]))) {
-                    while ((parseInt(lineCharacters[actualLine][actualPosition]))) {
-                        partialSum += lineCharacters[actualLine][actualPosition]
-                        actualPosition++
-                    }
-                    sum += parseInt(partialSum)
-                    partialSum = ""
-                }
-                if (lineCharacters[actualLine][actualPosition] === "\\") {
-                    direction = 4
-                    actualLine++
-                } else if (lineCharacters[actualLine][actualPosition] === "/") {
-                    direction = 3
-                    actualLine--
-                } else {
-                    actualPosition++
-                }
-                break
-            case 2:
-                if ((parseInt(lineCharacters[actualLine][actualPosition]))) {
-                    while ((parseInt(lineCharacters[actualLine][actualPosition]))) {
-                        partialSum += lineCharacters[actualLine][actualPosition]
-                        actualPosition--
-                    }
-                    sum += parseInt(partialSum)
-                    partialSum = ""
-                }
-                if (lineCharacters[actualLine][actualPosition] === "\\") {
-                    direction = 3
-                    actualLine--
-                } else if (lineCharacters[actualLine][actualPosition] === "/") {
-                    direction = 4
-                    actualLine++
-                } else {
-                    actualPosition--
-                }
-                break
-            case 3:
-                if ((parseInt(lineCharacters[actualLine][actualPosition]))) {
-                    while ((parseInt(lineCharacters[actualLine][actualPosition]))) {
-                        partialSum += lineCharacters[actualLine][actualPosition]
-                        actualLine--
-                    }
-                    sum += parseInt(partialSum)
-                    partialSum = ""
-                }
-                if (lineCharacters[actualLine][actualPosition] === "\\") {
-                    direction = 2
-                    actualPosition--
-                } else if (lineCharacters[actualLine][actualPosition] === "/") {
-                    direction = 1
-                    actualPosition++
-                } else {
-                    actualLine--
-                }
-                break
-            case 4:
-                if ((parseInt(lineCharacters[actualLine][actualPosition]))) {
-                    while ((parseInt(lineCharacters[actualLine][actualPosition]))) {
-                        partialSum += lineCharacters[actualLine][actualPosition]
-                        actualLine++
-                    }
-                    sum += parseInt(partialSum)
-                    partialSum = ""
-                }
-                if (lineCharacters[actualLine][actualPosition] === "\\") {
-                    direction = 1
-                    actualPosition++
-                } else if (lineCharacters[actualLine][actualPosition] === "/") {
-                    direction = 2
-                    actualPosition--
-                } else {
-                    actualLine++
-                }
-                break
-        }
-    }
-    console.log("Sum total = " + sum)
-    console.log("Tempo de leitura do arquivo: " + elapsedTime.toFixed(2) + " segundos")
-}
-
-readFile("labirinto2000.txt", executeCase)
+readFile("labirinto50.txt", executeCase)
